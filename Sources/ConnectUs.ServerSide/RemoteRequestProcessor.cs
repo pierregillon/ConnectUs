4b211815305fe7889f891333501a6142b6622ec1
Pierre GILLON <gillon.pierre@hotmail.fr>
2015-05-11 00:39:49 +0000

    Simply the logic on RemoteRequestProcessor for parallel execution.
    git-tfs-id: [https://pierregillon.visualstudio.com/DefaultCollection]$/ConnectUs;C348

--- Sources/ConnectUs.ServerSide/RemoteRequestProcessor.cs
+++ Sources/ConnectUs.ServerSide/RemoteRequestProcessor.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using ConnectUs.Business;
+﻿using ConnectUs.Business;
 using ConnectUs.Business.Connections;
 
 namespace ConnectUs.ServerSide
@@ -10,7 +6,6 @@ namespace ConnectUs.ServerSide
     public class RemoteRequestProcessor : IRequestProcessor
     {
         private readonly IConnection _connection;
-        private readonly Queue<RequestParameter> _requests = new Queue<RequestParameter>();
         private readonly object _locker = new object();
 
         // ----- Constructors
@@ -22,58 +17,20 @@ namespace ConnectUs.ServerSide
         // ----- Public methods
         public Response Process(Request request)
         {
-            var parameter = new RequestParameter(request);
-            EnqueueRequest(parameter);
-            parameter.WaitResponse();
-            if (parameter.Error != null) {
-                throw parameter.Error;
+            try {
+                lock (_locker) {
+                    _connection.Send(request);
+                    return _connection.Read<Response>();
+                }
             }
-            return parameter.Response;
+            catch (ConnectionException) {
+                _connection.Close();
+                throw;
+            }
         }
         public void Close()
         {
             _connection.Close();
-        }
-
-        // ----- Internal logics
-        private void EnqueueRequest(RequestParameter parameter)
-        {
-            lock (_locker) {
-                _requests.Enqueue(parameter);
-                if (_requests.Count == 1) {
-                    StartProcessingRequest();
-                }
-            }
-        }
-        private RequestParameter DequeueRequest()
-        {
-            return _requests.Dequeue();
-        }
-        private void StartProcessingRequest()
-        {
-            var thread = new Thread(ExecuteRequests);
-            thread.Start();
-        }
-        private void ExecuteRequests()
-        {
-            while (_requests.Any()) {
-                var parameter = DequeueRequest();
-                ExecuteRequest(parameter);
-            }
-        }
-        private void ExecuteRequest(RequestParameter parameter)
-        {
-            try {
-                _connection.Send(parameter.Request);
-                parameter.Response = _connection.Read<Response>();
-            }
-            catch (Exception e) {
-                parameter.Error = e;
-                _connection.Close();
-            }
-            finally {
-                parameter.Notify();
-            }
         }
     }
 }
