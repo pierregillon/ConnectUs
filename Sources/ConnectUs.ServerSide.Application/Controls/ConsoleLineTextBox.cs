1c289e3d82b9ed384b5076d21a88f9dec13ce08e
Pierre GILLON <gillon.pierre@hotmail.fr>
2015-05-13 15:57:25 +0000

    Start of the ConsoleLineTextBox
    git-tfs-id: [https://pierregillon.visualstudio.com/DefaultCollection]$/ConnectUs;C359

---
+++ Sources/ConnectUs.ServerSide.Application/Controls/ConsoleLineTextBox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ConnectUs.ServerSide.Application.Controls
+{
+    public class ConsoleLineTextBox : TextBox
+    {
+        private int _currentIndex = 0;
+        private const string StartChar = ">";
+
+        public override void BeginInit()
+        {
+            base.BeginInit();
+
+            Text = StartChar + " ";
+            _currentIndex = Text.Length;
+        }
+
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            if (char.IsLetterOrDigit((char) e.Key) && SelectionStart < _currentIndex) {
+                e.Handled = true;
+            }
+            base.OnPreviewKeyUp(e);
+
+            if (e.Key == Key.Enter) {
+                Text += Environment.NewLine + StartChar + " ";
+                _currentIndex = Text.Length;
+                SelectionStart = _currentIndex;
+            }
+        }
+    }
+}
