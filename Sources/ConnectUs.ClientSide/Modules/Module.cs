8e1b7a54c7ce9f3a9fac709030ef56e83157f6f9
Pierre GILLON <gillon.pierre@hotmail.fr>
2015-05-19 09:03:44 +0000

    Update the module loading by using assembly resolve on CurrentDomain.
    git-tfs-id: [https://pierregillon.visualstudio.com/DefaultCollection]$/ConnectUs;C367

--- Sources/ConnectUs.ClientSide/Modules/Module.cs
+++ Sources/ConnectUs.ClientSide/Modules/Module.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace ConnectUs.ClientSide.Modules
 {
@@ -9,32 +10,55 @@ namespace ConnectUs.ClientSide.Modules
         private readonly string _modulepath;
         private AppDomain _moduleDomain;
 
-        public string Path { get { return _modulepath; } }
+        public string Path
+        {
+            get { return _modulepath; }
+        }
         public string Name { get; private set; }
         public IEnumerable<object> Commands { get; private set; }
 
+        // ----- Constructors
         public Module(string modulepath)
         {
             _modulepath = modulepath;
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainOnAssemblyResolve;
         }
 
+        // ----- Public methods
         public void Load()
         {
-            _moduleDomain = AppDomain.CreateDomain("ModuleDomain", AppDomain.CurrentDomain.Evidence, new AppDomainSetup {ApplicationBase = Environment.CurrentDirectory});
-            var proxy = new Proxy();
-            var assembly = proxy.LoadAssembly(_modulepath);
-            var moduleType = assembly.GetTypes().FirstOrDefault(x => x.Name == "Module");
-            if (moduleType == null) {
-                throw new Exception(string.Format("Unable to instanciate module '{0}'.", assembly.FullName));
-            }
+            _moduleDomain = AppDomain.CreateDomain("ModuleDomain");
+            
+            var assembly = _moduleDomain.Load(AssemblyName.GetAssemblyName(_modulepath));
+            var moduleType = GetModuleFromAssembly(assembly);
 
             Name = assembly.GetName().Name;
-            var module = Activator.CreateInstance(moduleType);
-            Commands = (IEnumerable<object>) moduleType.GetMethod("GetCommands").Invoke(module, new object[0]);
+            Commands = GetCommands(moduleType);
         }
         public void Unload()
         {
             AppDomain.Unload(_moduleDomain);
+        }
+
+        // ----- Event callbacks
+        private Assembly CurrentDomainOnAssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return Assembly.LoadFile(_modulepath);
+        }
+
+        // ----- Internal logics
+        private static Type GetModuleFromAssembly(Assembly assembly)
+        {
+            var moduleType = assembly.GetTypes().FirstOrDefault(x => x.Name == "Module");
+            if (moduleType == null) {
+                throw new Exception(string.Format("Unable to instanciate module '{0}'.", assembly.FullName));
+            }
+            return moduleType;
+        }
+        private static IEnumerable<object> GetCommands(Type moduleType)
+        {
+            var module = Activator.CreateInstance(moduleType);
+            return (IEnumerable<object>) moduleType.GetMethod("GetCommands").Invoke(module, new object[0]);
         }
     }
 }
