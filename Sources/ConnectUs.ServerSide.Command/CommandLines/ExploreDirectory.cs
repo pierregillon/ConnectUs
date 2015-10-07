cb4eb42df288b4d19e882d412c0a16435e56fcdc
Pierre GILLON <gillon.pierre69@gmail.com>
2015-10-07 14:12:27 +0200

    Add the explorer folder command line.

---
+++ Sources/ConnectUs.ServerSide.Command/CommandLines/ExploreDirectory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using ConnectUs.FileExplorer;
+
+namespace ConnectUs.ServerSide.Command.CommandLines
+{
+    [CommandDescription(CommandName = "dir", Description = "Display the list of files and directories of a remote folder.")]
+    internal class ExploreDirectory : CurrentClientCommand
+    {
+        public ExploreDirectory(Context context)
+            : base(context)
+        {
+        }
+
+        protected override string HandleInternal(CommandLine commandLine, Client client)
+        {
+            var directoryPath = commandLine.Arguments.FirstOrDefault(x => x.Name == "unknown");
+            if (directoryPath == null) {
+                return "You should define the directory path.";
+            }
+
+            var request = new ExploreDirectoryRequest
+            {
+                DirectoryPath = directoryPath.Value,
+                GetDirectories = true,
+                GetFiles = true
+            };
+            var response = client.ExecuteCommand<ExploreDirectoryRequest, ExploreDirectoryResponse>(request);
+            return string.Join(Environment.NewLine, response.Files.Select(x => x.Name));
+        }
+    }
+}
