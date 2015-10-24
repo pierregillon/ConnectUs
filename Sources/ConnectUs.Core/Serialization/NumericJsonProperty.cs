cabc9f433c86a5aed27f879a00e9d3461f3179a0
Pierre GILLON <gillon.pierre69@gmail.com>
2015-10-24 13:32:40 +0200

    Refactor the get parse method for numeric.

--- Sources/ConnectUs.Core/Serialization/NumericJsonProperty.cs
+++ Sources/ConnectUs.Core/Serialization/NumericJsonProperty.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
@@ -20,27 +21,45 @@ namespace ConnectUs.Core.Serialization
 
         public object Materialize(Type type)
         {
+            var parseMethod = GetParseMethod(type);
+            if (parseMethod == null) {
+                throw new Exception("Unable to parse the value.");
+            }
+            if (parseMethod.GetParameters().Length == 2) {
+                return parseMethod.Invoke(null, new object[] {_json, CultureInfo.InvariantCulture});
+            }
+            else {
+                return parseMethod.Invoke(null, new object[] {_json});
+            }
+        }
+        public override string ToString()
+        {
+            return _json;
+        }
+
+        private MethodInfo GetParseMethod(Type type)
+        {
             var parseMethods = type
                 .GetMethods(BindingFlags.Static | BindingFlags.Public)
                 .Where(x => x.Name == "Parse")
                 .ToArray();
 
-            var parseMethod = parseMethods.SingleOrDefault(x => x.GetParameters().Count() == 2 && x.GetParameters().Last().ParameterType == typeof (IFormatProvider));
-            if (parseMethod != null) {
-                return parseMethod.Invoke(null, new object[] { _json, CultureInfo.InvariantCulture });
-            }
-            else {
-                parseMethod = parseMethods.SingleOrDefault(x => x.GetParameters().Count() == 1);
-                if (parseMethod == null) {
-                    throw new Exception("Unable to parse the value.");
-                }
-                return parseMethod.Invoke(null, new object[] { _json });
-            }
+            return GetParseMethodWithCulture(parseMethods) ?? GetParseMethods(parseMethods);
         }
-
-        public override string ToString()
+        private static MethodInfo GetParseMethods(IEnumerable<MethodInfo> parseMethods)
         {
-            return _json;
+            return parseMethods.SingleOrDefault(x => x.GetParameters().Count() == 1);
+        }
+        private static MethodInfo GetParseMethodWithCulture(IEnumerable<MethodInfo> parseMethods)
+        {
+            return parseMethods
+                .Where(x =>
+                {
+                    var parameters = x.GetParameters();
+                    return parameters.Count() == 2 &&
+                           parameters.Last().ParameterType == typeof (IFormatProvider);
+                })
+                .SingleOrDefault();
         }
     }
 }
